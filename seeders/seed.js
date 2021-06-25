023251fb496e8f2bca90c4e203cd210a3c529e20
Melanie <melanie.elson@icloud.com>
2021-06-24 18:30:05 -0600

    update

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Workout = require('../models/Workout');
+const Workout = require('../models/Workouts');
 require("dotenv").config();
 mongoose.connect('mongodb://localhost/workout', {
   useNewUrlParser: true,
